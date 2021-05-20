34492d401589ec1ce3782da1353b2ef72d6dcd76
sanjoli63 <sanjoligo6mar@gmail.com>
2021-05-20 11:50:28 +0530

    fc

--- lib/main.dart
+++ lib/main.dart
@@ -5,37 +5,27 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'MyShop',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-
+class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body:
-    );
+        appBar: AppBar(
+          title: Text('MyShop'),
+        ),
+        body: Center(
+          child: Text('Let\'s build a shop!'),
+        ));
   }
 }

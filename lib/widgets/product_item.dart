f694ff0924337acf2461d2026011dcf71c661917
sanjoli63 <sanjoligo6mar@gmail.com>
2021-06-12 13:28:55 +0530

    accent color deprecated' q

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -34,7 +34,7 @@ class ProductItem extends StatelessWidget {
             icon: Icon(
               product.isFavorite ? Icons.favorite : Icons.favorite_border,
             ),
-            color: Theme.of(context).accentColor,
+            color: Theme.of(context).colorScheme.secondary,
             onPressed: () {
               product.toggleFavoriteStatus();
             },
@@ -48,7 +48,7 @@ class ProductItem extends StatelessWidget {
               Icons.shopping_cart,
             ),
             onPressed: () {},
-            color: Theme.of(context).accentColor,
+            color: Theme.of(context).colorScheme.secondary,
           ),
         ),
       ),
